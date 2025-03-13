292b2be42802940730a2321263328b61cde1c3a2
JovisFilius <42693100+JovisFilius@users.noreply.github.com>
2025-03-13 14:24:31 +0100

    run_psths edited

--- trace_c4/matlab/run_psths.m
+++ trace_c4/matlab/run_psths.m
@@ -1,4 +1,4 @@
-mouse_names = [ "Quimper", "Jackson", "Flint", "Greene", "Houston", "Iowa", "Missouri", "Newark", "Orleans", "Pittsburg","Quimper", "Uppsala", "ReserveMouse3", "Seattle", "Madrid", "Zurich", "Yosemite", "Lisbon", "York", "Xanthi", "Amsterdam", "Ana2", "Ana4", "Ana5", "Copenhagen", "Istanbul", "Kyiv", "Porto", "Queens", "Reno", "Rotterdam", "Tallinn", "Venice", "Willemstad", "Zachary", "Dallas"];
+mouse_names = ["Jackson", "Flint", "Greene", "Houston", "Iowa", "Missouri", "Newark", "Orleans", "Pittsburg","Quimper", "Uppsala", "ReserveMouse3", "Seattle", "Madrid", "Zurich", "Yosemite", "Lisbon", "York", "Xanthi", "Amsterdam", "Ana2", "Ana4", "Ana5", "Copenhagen", "Istanbul", "Kyiv", "Porto", "Queens", "Reno", "Rotterdam", "Tallinn", "Venice", "Willemstad", "Zachary", "Dallas"];
 %mouse_names = ["Ana4"];
 
 for m = 1:length(mouse_names)
