13b3bb2dc440936898926f002d19406582b9ef62
ilse <125893237+iklinkhamer@users.noreply.github.com>
2025-03-14 14:20:07 +0100

    edited mouse_names

--- trace_c4/matlab/run_psths.m
+++ trace_c4/matlab/run_psths.m
@@ -1,6 +1,6 @@
-mouse_names = [ "Dallas", "Uppsala", "ReserveMouse3", "Seattle", "Madrid", "Zurich", "Yosemite", "Lisbon", "York", "Xanthi", "Amsterdam", "Ana2", "Ana4", "Ana5", "Copenhagen", "Istanbul", "Kyiv", "Porto", "Queens", "Reno", "Rotterdam", "Tallinn", "Venice", "Willemstad", "Zachary"];
+mouse_names = [ "Ana2", "Ana4", "Ana5", "Copenhagen", "Istanbul", "Kyiv", "Porto", "Queens", "Reno", "Rotterdam", "Tallinn", "Venice", "Willemstad", "Zachary"];
 %mouse_names = ["Ana4"];
-% done = ["Iowa", "Jackson", "Flint", "Greene", "Houston",  "Missouri", "Newark", "Orleans", "Pittsburg","Quimper",]
+% done = ["Iowa", "Jackson", "Flint", "Greene", "Houston",  "Missouri", "Newark", "Orleans", "Pittsburg","Quimper", "Dallas", "Uppsala", "ReserveMouse3", "Seattle", "Madrid", "Zurich", "Yosemite", "Lisbon", "York", "Xanthi", "Amsterdam", ]
 
 for m = 1:length(mouse_names)
     try
