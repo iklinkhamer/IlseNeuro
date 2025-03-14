724e57bd08d82156383e8d264b3ea54a5eaf075a
ilse <125893237+iklinkhamer@users.noreply.github.com>
2025-03-14 16:20:35 +0100

    Changed mouse name order

--- trace_c4/matlab/run_psths.m
+++ trace_c4/matlab/run_psths.m
@@ -1,6 +1,6 @@
-mouse_names = [ "Ana2", "Ana4", "Ana5", "Copenhagen", "Istanbul", "Kyiv", "Porto", "Queens", "Reno", "Rotterdam", "Tallinn", "Venice", "Willemstad", "Zachary"];
+mouse_names = ["Iowa", "Jackson", "Flint", "Greene", "Houston",  "Missouri", "Newark", "Orleans", "Pittsburg","Quimper", "Dallas", "Uppsala", "ReserveMouse3", "Seattle", "Madrid", "Zurich", "Yosemite", "Lisbon", "York", "Xanthi",  "Ana2", "Ana4", "Ana5", "Copenhagen", "Istanbul", "Kyiv", "Porto", "Queens", "Reno", "Rotterdam", "Tallinn", "Venice", "Willemstad", "Zachary", "Amsterdam"];
 %mouse_names = ["Ana4"];
-% done = ["Iowa", "Jackson", "Flint", "Greene", "Houston",  "Missouri", "Newark", "Orleans", "Pittsburg","Quimper", "Dallas", "Uppsala", "ReserveMouse3", "Seattle", "Madrid", "Zurich", "Yosemite", "Lisbon", "York", "Xanthi", "Amsterdam", ]
+% done = []
 
 for m = 1:length(mouse_names)
     try
